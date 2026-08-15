89a6c3d17893850af750e0c76bc37b28dcc11ffa
unknown <andreiacojido@gmail.com>
2026-08-15 11:24:28 +0800

    modal form additional skeleton inputs

--- src/modal.js
+++ src/modal.js
@@ -31,6 +31,50 @@ export class Modal {
     const descriptionInput = document.createElement("textarea");
     descriptionInput.id = "todo-description";
     descriptionInput.required = true;
+    
+
+    const deadlineLabel = document.createElement("label");
+    deadlineLabel.textContent = "Add a deadline";
+    deadlineLabel.htmlFor ="deadline-input";
+    
+    const deadlineInput = document.createElement("input");
+    deadlineInput.type = "date";
+    deadlineInput.id = "deadline-input";
+
+    const priorityLabel = document.createElement("label");
+priorityLabel.textContent = "Set a priority level";
+
+const lowPriority = document.createElement("input");
+lowPriority.type = "radio";
+lowPriority.name = "priority";
+lowPriority.value = "low";
+lowPriority.id = "priority-low";
+
+const lowLabel = document.createElement("label");
+lowLabel.textContent = "Low";
+lowLabel.htmlFor = "priority-low";
+
+
+const mediumPriority = document.createElement("input");
+mediumPriority.type = "radio";
+mediumPriority.name = "priority";
+mediumPriority.value = "medium";
+mediumPriority.id = "priority-medium";
+
+const mediumLabel = document.createElement("label");
+mediumLabel.textContent = "Medium";
+mediumLabel.htmlFor = "priority-medium";
+
+
+const highPriority = document.createElement("input");
+highPriority.type = "radio";
+highPriority.name = "priority";
+highPriority.value = "high";
+highPriority.id = "priority-high";
+
+const highLabel = document.createElement("label");
+highLabel.textContent = "High";
+highLabel.htmlFor = "priority-high";
 
     const cancelBtn = document.createElement("button");
     cancelBtn.type = "button";
@@ -40,7 +84,9 @@ export class Modal {
     submitBtn.type = "submit";
     submitBtn.textContent = "Add Task";
 
-
+const footer = document.createElement("div");
+footer.id = "modal-footer";
+footer.append(cancelBtn, submitBtn);
 
     form.addEventListener("submit", (event)=> {
         event.preventDefault();
@@ -63,8 +109,21 @@ export class Modal {
       titleInput,
       descriptionLabel,
       descriptionInput,
-      cancelBtn,
-      submitBtn
+       deadlineLabel,
+    deadlineInput,
+     
+      priorityLabel,
+
+    lowPriority,
+    lowLabel,
+
+    mediumPriority,
+    mediumLabel,
+
+    highPriority,
+    highLabel, 
+   
+    footer
     );
 
     dialog.append(form);
